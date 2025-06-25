26b81746c030411a0f7f57c936932be820778ac1
Filip Florczyk <filip.florczyk@n7space.com>
2025-06-25 11:04:28 +0200

    Fixed import script

--- component/cfdp/work/cfdp/importScript.sh
+++ component/cfdp/work/cfdp/importScript.sh
@@ -6,10 +6,10 @@ if [[ -f "$filepath" ]]; then
     user_input=$(<"$filepath")
     echo "You entered: $user_input"
     user_input="${user_input//-/_}"
-    sed -i "s/APP_MARKER/$user_input/" C/src/filesystem.c
+    sed -i "s/APP_MARKER/$user_input/" C/src/cfdp.c
     user_input="${user_input//_/-}"
-    sed -i "s/APP-MARKER/$user_input/" ../../filesystem.asn
-    sed -i "s/APP-MARKER/$user_input/" ../../filesystem.acn
+    sed -i "s/APP-MARKER/$user_input/" ../../taste-cfdp.asn
+    sed -i "s/APP-MARKER/$user_input/" ../../taste-cfdp.acn
     rm -f "$filepath"
 fi
 
